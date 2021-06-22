8792d737647dccacd00f6cea51f36bce9f1558ff
Alisson Perim Honostorio <alissonhonostorio@gmail.com>
2021-06-22 19:25:57 -0300

    Feature: criado arquivo mockupForNews.js com o inicio do mockup

---
+++ ny/group2/js/mockupForNews.js
@@ -0,0 +1,359 @@
+const NEWS_LIST = [
+    {
+        firstPageNews: [
+            {
+                title: "Lorem ipsum dolor sit amet",
+                subtitle: "Lorem ipsum dolor sit amet",
+                info: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Doloribus aliquid doloremque nihil quaerat, deserunt
+                repellendus a officia distinctio. Commodi dolorem iure
+                blanditiis in at animi quae perspiciatis praesentium eius
+                est non molestiae quo labore maiores dolorum quam
+                sequi, et numquam.`,
+                info2: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Doloribus aliquid doloremque nihil quaerat, deserunt
+                repellendus a officia distinctio. Commodi dolorem iure
+                blanditiis in at animi quae perspiciatis praesentium eius
+                est non molestiae quo labore maiores dolorum quam
+                sequi, et numquam.`
+            },
+            {
+                title: "Lorem ipsum dolor sit amet",
+                subtitle: "Lorem ipsum dolor sit amet",
+                info: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Doloribus aliquid doloremque nihil quaerat, deserunt
+                repellendus a officia distinctio. Commodi dolorem iure
+                blanditiis in at animi quae perspiciatis praesentium eius
+                est non molestiae quo labore maiores dolorum quam
+                sequi, et numquam.`,
+                info2: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Doloribus aliquid doloremque nihil quaerat, deserunt
+                repellendus a officia distinctio. Commodi dolorem iure
+                blanditiis in at animi quae perspiciatis praesentium eius
+                est non molestiae quo labore maiores dolorum quam
+                sequi, et numquam.`
+            },
+        ],
+
+        liveUpdatesImages: [
+            {
+                imageToUpdate: "imagpicstum"
+            },
+            {
+                imageToUpdate: "imagpicstum"
+            },
+            {
+                imageToUpdate: "imagpicstum"
+            },
+            {
+                imageToUpdate: "imagpicstum"
+            },
+            {
+                imageToUpdate: "imagpicstum"
+            },
+            {
+                imageToUpdate: "imagpicstum"
+            },
+            {
+                imageToUpdate: "imagpicstum"
+            },
+        ],
+
+        liveUpdatePrimaries: [
+            {
+                time: "25m ago",
+                news: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+            },
+            {
+                time: "25m ago",
+                news: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+            },
+            {
+                time: "25m ago",
+                news: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+            }
+        ],
+
+        someOtherNews: [
+            {
+                title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+                news: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Doloribus aliquid doloremque nihil quaerat, deserunt
+                repellendus a officia distinctio. Commodi dolorem iure
+                blanditiis in at animi quae perspiciatis praesentium eius
+                est non molestiae quo labore maiores dolorum quam
+                sequi, et numquam.`
+            },
+            {
+                title: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+            }
+        ],
+
+        liveNews: [
+            {
+                title: "Senat Votin Rights Bill Expected to Run Into Filibuster",
+                subtitle: "",
+                news: `Democrats plan to call a test vote to try to advance a sweeping
+                voting rights bill. They are all but certain to fall. Here's the latest
+                from Washington.`,
+                image: "",
+                alt: "Some Woman Looking forward"
+            },
+            {
+                title: `A large liberal super PAC will pour $20 million into voting
+                rights efforts ahead of the 2022 election cycle.`,
+                subtitle: ``,
+                news: "",
+                image: "",
+                alt: "Some Woman Looking forward"
+            },
+            {
+                title: "She Fought to Reopen Schools, Becoming a Hero and a Villain",
+                subtitle: ``,
+                news: `The Economist Emily Oster offers loads of data-driven advice
+                about children and Covid-19. Many parents live by her words. Others
+                say she's dangerous`,
+                image: "Lorem picsum image",
+                alt: "Some Woman Looking forward"
+            },
+            {
+                title: "India Tries to Jump Start Sluggish Vaccination Effort",
+                subtitle: "",
+                news: `The country administered 8.6 million doses of vaccine
+                on Monday, setting a national record on the first day of a new
+                policy. Get the latest on Covid-19.`,
+                image: "",
+                alt: "Some Woman Looking forward"
+            },
+            {
+                title: `While many people in th U.S. need persuading to get
+                vaccinated, residents of South Texas need little prodding.`,
+                subtitle: ``,
+                news: ``,
+                image: "",
+                alt: ""
+            },
+        ],
+
+        thirdSectionNews: [
+            {
+                title: `Britney Spears Quietly Pushed for Years to End Her
+                Conservatorship`,
+                subtitle: "",
+                news: `Records obtained by The Times reveal that the singer
+                has urged changesto the arrangement that controls her life,
+                and her father’s role in leading it.`,
+                image: "Lorem Picsum",
+                alt: "Britney driving with her father."
+            },
+            {
+                title: `Los Angeles Has a Housing Crisis. Can It Design
+                Its Way Out?`,
+                subtitle: "",
+                news: `As Angelenos grapple with ways to address affordable
+                housing and homelessness, a new architecture competition
+                looks to low-rise solutions, our critic writes.`,
+                image: "Lorem Picsum",
+                alt: `Louisa Van Leer and Antonio Castillo won first place in
+                an architecture competition in Los Angeles for their “Green
+                Alley Housing” proposal in the subdivision category. Louisa
+                Van Leer Architecture`
+            },
+            {
+                title: `An Enginer Says He's Cracked the Zodiac. Online Sleuths
+                Are Skeptical.`,
+                subtitle: "",
+                news: `Fayçal Ziraoui said he cracked ciphers attributed to
+                the Zodiac killer and identified him, causing an uproar and
+                potentially ending a 50-year-old quest.`,
+                image: "Lorem Picsum",
+                alt: "Some man looking for the right side, with a jar on the table."
+            },
+            {
+                title: `Carl Nassib, the First Openly Gay N.F.L. Player, Has
+                Been a Football Everyman`,
+                subtitle: "",
+                news: `I’ve been meaning to do this for a while now,” the lineman
+                said of coming out. He comes from a football family and was a
+                standout at Penn State.`,
+                image: "Lorem Picsum",
+                alt: "Carl Nassib image."
+            },
+        ],
+
+        moreNews: [
+            {
+                title: `Against Expectations, Southwestern Summers Are Getting
+                Even Drier`,
+                subtitle: "",
+                news: `The finding by researchers runs counter to a basic tenet
+                of climate change — that warming increases humidity. It’s also
+                bad news for fire seasons.`,
+                image: "Lorem Picsum",
+                alt: "Dry plantation."
+            },
+            {
+                title: ``,
+                subtitle: `Mark Peel, Who Helped Forge a New Culinary Path, Dies at 66`,
+                news: `A pillar of California’s pioneering food scene, he worked
+                at Spago and was a founder of the renowned Campanile and La Brea Bakery.`,
+                image: ``,
+                alt: ``
+            },
+            {
+                title: ``,
+                subtitle: `The Pandemic Stimulus Was Front-Loaded. That Could Mean a Bumpy Year.`,
+                news: `There’s a risk that the affluent, who saved money by limiting things like
+                traveling and dining out, will sit on their cash rather than spend it.`,
+                image: ``,
+                alt: ``
+            },
+            {
+                title: ``,
+                subtitle: `Home Sales Slow as Shortage and Climbing Prices Limit Buyers.`,
+                news: `Buyers continued to leave cities in search of more space and
+                better work-from-home scenarios, leading to a spike in demand. Here’s
+                the latest on the economy.`,
+                image: ``,
+                alt: ``
+            }
+        ],
+
+        rightSideMoreNews: [
+            {
+                title: ``,
+                subtitle: `Connecticut Legalizes Recreational Marijuana`,
+                news: ``,
+                image: ``,
+                alt: ``
+            },
+            {
+                title: ``,
+                subtitle: `Google Faces Fresh E.U. Inquiry Over Ad Technology`,
+                news: ``,
+                image: ``,
+                alt: ``
+            },
+            {
+                title: ``,
+                subtitle: `European Soccer Says No to Pride Display at Munich Stadium`,
+                news: ``,
+                image: ``,
+                alt: ``
+            },
+            {
+                title: ``,
+                subtitle: `Resistance Fighters Battle Myanmar’s Military in Mandalay`,
+                news: ``,
+                image: ``,
+                alt: ``
+            },
+            {
+                title: ``,
+                subtitle: `Man Charged With Stealing 21 Tons of Pistachios in California`,
+                news: ``,
+                image: ``,
+                alt: ``
+            },
+        ],
+
+        paidPost: {
+            author: `AUDEMARS PIGUET`,
+            title: `These Artists Are Innovating the Age-Old Craft of Ceramics`,
+            subtitle: `Meet the ceramicists Aneta Regel, Andile Dyalvane and Shigekazu Nagae.`,
+            authorsImage: `Lorem picsum`,
+            alt: `Some chinese old guy looking forward`,
+            logoImage: `Some Logo Image`
+        },
+
+        womansHealth: [
+            {
+                image: `perimenopause`,
+                title: `Why Is Perimenopause Still Such a Mystery?`
+            },
+            {
+                image: `uterus cartoon with woman's face`,
+                title: `Know Your Uterus`
+            },
+            {
+                image: `some woman thinking about the issues from postpartumn`,
+                title: `Postpartumn Body Changes You Should Know About`
+            },
+            {
+                image: `some cartoons head, with brains like a sava`,
+                title: `This Is Your Brain on Motherhood`
+            },
+            {
+                image: `some woman and some x-ray`,
+                title: `when Doctor's Downplay Woman's Health Concerns`
+            },
+        ],
+
+        featuresMain:
+        {
+            image: `Pan American Unity, a 30-ton, 74-foot-wide-by-22-foot
+                mural by Diego Rivera, being installed at SFMOMA as the culmination
+                of a four-year, multimillion-dollar undertaking involving
+                engineers, architects, art historians, art handlers and riggers
+                from the United States and Mexico. It will be on view starting Monday.`,
+            title: `How Do You Move a 30-ton Diego Rivera Fresco?`,
+            subtitle: `Diego Rivera's rarely seen "Pan American Unity" was carefully
+                extracted from its home at City College of San Francisco and moved across
+                town to SFMOMA`
+        },
+
+        featuresRightSide: [
+            {
+                image: `3 Hollywood Stars sited and smilling`,
+                title: `3 Hollywood Stars Recas Their Lives in Austin`,
+                subtitle: `As the pandemic upended Tinseltown, Jamie-Lynn Sigler,
+                Haylie Duff and Becca Tobin made a pact to join the mass migration
+                from California to Texas.`
+            },
+            {
+                image: `some lipsticks in the photo`,
+                title: `Does the Perfect 'MLBB' Lipstick exist?`,
+                subtitle: `As the masks come off, the search for the perfect
+                “my lips but better” shade continues, perhaps into perpetuity..`
+            },
+            {
+                image: `some old book named Americanon`,
+                title: `Review: America's Character, as Seen Its How-To Books`,
+                subtitle: `In “Americanon,” Jess McHugh catalogs the country’s
+                virtues and foibles by examining the history of almanacs,
+                dictionaries and other practical books.`
+            },
+            {
+                image: `a woman's cartoon holding his soons, computer and calendar`,
+                title: `Being ‘Always On’ Has Reached Its Limits, and Parents Need a Reset`,
+                subtitle: `What crisis-acquired habits should working parents break?
+                Daisy Dowling, the author of “Workparent,” weighs in.`
+            },
+        ],
+
+        nytCooking: [
+            {
+                image: `gazpacho`,
+                title: `Best Gazpacho`
+            },
+            {
+                image: `dutch baby`,
+                title: `Dutch Baby With Bacon and Runny Camembert`
+            },
+            {
+                image: `tofu and green beens`,
+                title: `Tofu and Green Beens With Chile Crisp`
+            },
+            {
+                image: `fresh strawberry`,
+                title: `Fresh Strawberry Bundt Cake`
+            },
+            {
+                image: `jalapeño`,
+                title: `Jalapeño Grilled Pork Chops`
+            },
+        ],
+
+
+    }
+]
