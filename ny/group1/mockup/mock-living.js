24f345f839939f89000ca194d2fcd9835010b0c0
Lucas Barbosa Dias <lucasbarbosadias@outlook.com>
2021-06-23 17:52:10 -0300

    feature: add mocks and gitignore

---
+++ ny/group1/mockup/mock-living.js
@@ -0,0 +1,34 @@
+export const living = [
+    {
+        title: "Style",
+        news: ["Naomi Osaka and the Language of Fame", "Symone Is a Love Letter to Blackness and Queerness", "The End of the Wonder Rug"]
+    },
+    {
+        title: "Food",
+        news: ["More Than ‘Just Takeout’", "French Fries and Chapli Kebabs, Hot From the Skillet", "Grocery-to-Table Is a Challenge for Restaurants in the Pandemic"]
+    },
+    {
+        title: "Well",
+        news: ["The Secrets of ‘Cognitive Super-Agers’", "Morning People May Be at Lower Risk of Depression", "Than Night Owls New Homes, Pets and Dreams: Big Pandemic Changes"]
+    },
+    {
+        title: "The New York Times Magazine",
+        news: ["What if American Democracy Fails the Climate Crisis?", "The Best Way to Clean Your Ears: With a Spoon Should", "I Hang Out With Someone Whose Political Views I Hate?"]
+    },
+    {
+        title: "T Magazine",
+        news: ["Lucy Dacus Takes Confessional Songwriting to a New Level", "When Bots and Antibodies Are Art Materials", "Brigid Berlin, Andy Warhol’s Most Enduring Friend"]
+    },
+    {
+        title: "Travel",
+        news: ["It’s Summer in the Ski Towns, 2.0", "Touring Alaska in an R.V.", "Five Things to Know if You’re Road-Tripping This Summer"]
+    },
+    {
+        title: "Love",
+        news: ["This Season’s Wedding Crashers: Cicadas", "Finding Light Together After a ‘Very Dark Place’ My 70-Year-Old Father Joined Tinder"]
+    },
+    {
+        title: "Real Estate",
+        news: ["Three Hollywood Stars Recast Their Lives", "Deep in the Heart of Texas Why the Best Gardens", "Have Something Extra Getting Married and Leaving a Roommate Behind"]
+    }
+]
---
+++ ny/group1/mockup/mock-news.js
@@ -0,0 +1,58 @@
+export const news = [
+    {
+        title: "World News",
+        news: ["Covid Surge in Africa Raises Fears of a Calamity Like India’s", "I’ve Cracked Zodiac, a French Engineer Says.","Online Sleuths Are Skeptical. They Voted for Brexit, but not the Giant Truck Park That Came With It"]
+    },
+    {
+        title: "U.S News",
+        news: ["She Fought to Reopen Schools, Becoming a Hero and a Villain", "California Has a Plan to Pay the Back Rent for Low-Income Tenants. All of It.", "Many Parts of the U.S. Needed Persuading to Get Vaccinated. Not South Texas."]
+    },
+    {
+        title: "U.S. Politics",
+        news: ["White House Unveils Strategy to Combat Domestic Extremism", "How an Anti-Corruption Bill Became a Showdown on Democracy", "The White House publicly acknowledges the U.S. is likely to miss Biden’s July 4 vaccination goal."]
+    },
+    {
+        title: "New York",
+        news: ["Live Updates: Voters Turn Out Across N.Y.C. in Pivotal Race for Mayor", "New Yorkers Vote for Mayor in Race Tinged With Acrimony and Uncertainty", "Connecticut Legalizes Recreational Marijuana, with Sales Aimed for 2022"]
+    },
+    {
+        title: "Business",
+        news: ["Banks Slowly Offer Alternatives to Overdraft Fees, a Bane of Struggling Spenders Mobile Home", "Owners Fear Evictions as Pandemic Protections End Google" ,"Executives See Cracks in Their Company’s Success"]
+    },
+    {
+        title: "Technology",
+        news: ["Google’s internet advertising dominance draws a fresh E.U. antitrust inquiry.", "Google Executives See Cracks in Their Company’s Success", "Inside the ‘Deadly Serious’ World of E-Sports in South Korea"]
+    },
+    {
+        title: "Science",
+        news: ["When an Eel Climbs a Ramp to Eat Squid From a Clamp, That’s a Moray", "Why Geology Is Our Destiny", "How Glowing Bacteria in the Dirt May One Day Save Lives"]
+    },
+    {
+        title: "Sports",
+        news: ["‘Glove, Hat and Belt’: M.L.B. Begins a New Era of Enforcement", "China’s Sun Yang Barred for Four Years", "Players of Asian Descent on the L.P.G.A. Tour Lift Silence on Racism and Sexism"]
+    },
+    {
+        title: "Obituaries",
+        news: ["Sylvia Deutsch, a Force in New York City Land Use, Dies at 96", "Mark Peel, Who Helped Forge a New Culinary", "Path, Dies at 66 Consuewella Africa, 67, Dies; Lost Two Daughters in MOVE Siege"]
+    },
+    {
+        title: "The Upshot",
+        news: ["New Drug Could Cost the Government as Much as It Spends on NASA", "Medicaid Enrollment Surpassed 80 Million, a Record, During the Pandemic", "The Pandemic Stimulus Was Front-Loaded. That Could Mean a Bumpy Year."]
+    },
+    {
+        title: "Climate and Environment",
+        news: ["Against Expectations, Southwestern Summers Are Getting Even Drier", "U.N. Says Great Barrier Reef Is ‘in Danger.’", "Australia Bitterly Disagrees. Climate Change Batters the West Before Summer Even Begins"]
+    },
+    {
+        title: "Education",
+        news: ["Does It Hurt Children to Measure Pandemic Learning Loss?", "Online Schools Are Here to Stay, Even After the Pandemic", "Why Students Are Logging In to Class From 7,000 Miles Away"]
+    },
+    {
+        title: "Health",
+        news: ["Many Alzheimer’s Experts Say Use of Aduhelm Should Be Sharply Limited", "Medicaid Enrollment Surpassed 80 Million, a Record, During the Pandemic", "Wondering if the Vaccine Worked? Get the Right Test, at the Right Time"]
+    },
+    {
+        title: "Reader Center",
+        news: ["Behind One Pulitzer, Hundreds of Hands", "I’ve Recovered From Long Covid. I’m One of the Lucky Ones.", "A Dad and an Audience of One"]
+    }
+]
