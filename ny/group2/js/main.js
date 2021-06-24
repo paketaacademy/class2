4dfd9ef796583622adfdd71b2b444deef3827bbc
Alisson Perim Honostorio <alissonhonostorio@gmail.com>
2021-06-24 11:02:59 -0300

    Feature: Criado arquivo main.js para criar as funções de javascript, adicina elementos dinamicamente no html e cria novos estilos no arquivo style.css

---
+++ ny/group2/js/main.js
@@ -0,0 +1,64 @@
+import { NEWS_LIST } from "./mockupForNews.js";
+
+const mock = NEWS_LIST
+
+let principalNews = document.querySelector(".principal-news")
+let liveUpdatesImg = document.querySelectorAll(".live-updates__ul--img")
+let liveUpdatePrimaries = document.querySelector(".live-update-primaries")
+let someOtherNews = document.querySelector(".some-other-news")
+
+const firstPageNew = () => {
+    mock.map(li => (
+        li.firstPageNews.map(a => (
+            principalNews.innerHTML = `
+                <h2 class="principal-news__h2">${a.title}</h2>
+                <h3 class="principal-news__h3">${a.subtitle}</h3>
+                <ul class="principal-news__ul">
+                    <li class="principal-news__ul--li">${a.info}</li>
+                    <li class="principal-news__ul--li">${a.info2}</li>
+                </ul>
+            `
+        ))
+    ))
+}
+
+const genereteImages = () => {
+    mock.map(m => (
+        m.liveUpdatesImages.forEach((x, index) => {
+            liveUpdatesImg[index].setAttribute('src', x.imageToUpdate)
+            liveUpdatesImg[index].setAttribute('alt', x.alt)
+        })
+    ))
+}
+
+const generateLiveUpdatesPrimaries = () => {
+    mock.map(m => (
+        m.liveUpdatePrimaries.map(l => (
+            liveUpdatePrimaries.innerHTML += `
+                <p class="live-update-primaries__p">
+                    <span class="live-update-primaries__span">
+                        ${l.time}
+                    </span>
+                    ${l.news}
+                </p>
+            `
+        ))
+    ))
+}
+
+const generateSomeOtherNews = () => {
+    mock.map(m => (
+        m.someOtherNews.map(s => (
+            someOtherNews.innerHTML += `
+                <h2 class="some-other-news__h2">${s.title}</h2>
+                <p class="some-other-news__p">${s.news}</p>
+                <div class="some-other-news-divider"></div>
+            `
+        ))
+    ))
+}
+
+firstPageNew()
+genereteImages()
+generateLiveUpdatesPrimaries()
+generateSomeOtherNews()
