75f204182597edc82814b619128e9a305a79c55b
Lucas Barbosa Dias <lucasbarbosadias@outlook.com>
2021-06-25 14:30:05 -0300

    chore: adding footer in js

---
+++ ny/group1/mockup/mock-footer.js
@@ -0,0 +1,74 @@
+export const newsItems = [
+    "Home Page",
+    "World",
+    "Coronavirus",
+    "U.S.",
+    "Politics",
+    "New York",
+    "Busines",
+    "Tech",
+    "Science",
+    "Sports",
+    "Obituaries",
+    "Today's Paper",
+    "Corrections",
+    "Trending"
+]
+    
+export const opinionItems = [
+    "Today's Opinion",
+    "Columnists",
+    "Editorials",
+    "Guest Essays",
+    "Letters",
+    "Sunday Review",
+    "Video: Opinion",
+    "Video: Opinion"
+]
+   
+export const artsItems = [
+    "Today's Arts",
+    "Art & Design",
+    "Books",
+    "Best Sellers Book List",
+    "Dance",
+    "Movies",
+    "Music",
+    "Pop Culture",
+    "Television",
+    "Theater",
+    "Video: Arts"
+]
+   
+export const livingItems = [
+    "Automotive",
+    "Games",
+    "Education",
+    "Food",
+    "Health",
+    "Jobs",
+    "Love",
+    "Magazine",
+    "Parenting",
+    "Real Estate",
+    "Style",
+    "T Magazine",
+    "Travel"
+]
+    
+export const moreItems = [
+    "Reader Center",
+    "Wirecutter",
+    "Cooking",
+    "Live Events",
+    "The Learning Network",
+    "Tools & Services",
+    "Podcasts",
+    "Multimedia",
+    "Photography",
+    "Video",
+    "TimesMachine",
+    "NYT Store",
+    "Manage My Account",
+    "NYTLicensing"
+]
