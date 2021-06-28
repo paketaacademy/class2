cf5aee4de8e3657bb48125895e6cfec2544062ae
Lucas Barbosa Dias <lucasbarbosadias@outlook.com>
2021-06-28 13:35:56 -0300

    feature: add scroll in index.html

---
+++ ny/group1/js/post.js
@@ -0,0 +1,3 @@
+import { init } from "../js/footer.js"
+
+init()
--- ny/group1/js/script.js
+++ ny/group1/js/script.js
@@ -3,6 +3,7 @@ import { news } from "../mockup/mock-news.js"
 import { arts } from "../mockup/mock-arts.js"
 import { living } from "../mockup/mock-living.js"
 import { opinion } from "../mockup/mock-opinion.js"
+import { init } from "../js/footer.js"
 
 const factoryNews = (array, div) => {
     array.map( (a) => {
@@ -36,15 +37,41 @@ const factoryPopular = (array, div) => {
     })
 }
 
-
 let popDiv = document.querySelector('#pop')
 let newsDiv = document.querySelector('#news')
 let opinionDiv = document.querySelector('#opinion')
 let artsDiv = document.querySelector('#arts')
 let livingDiv = document.querySelector('#living')
+let cont = 0
+let titlePopular = document.querySelector('.title__popular')
+let titleNews = document.querySelector('.title__news')
+let titleOpinion = document.querySelector('.title__opinion')
+let titleArts = document.querySelector('.title__arts')
+let titleLiving = document.querySelector('.title__living')
 
-factoryPopular(pop, popDiv)
-factoryNews(news, newsDiv)
-factoryNews(opinion, opinionDiv)
-factoryNews(arts, artsDiv)
-factoryNews(living, livingDiv)
+window.addEventListener('scroll', () => {	
+	const { scrollTop, scrollHeight, clientHeight } = document.documentElement;	
+	if(scrollTop + clientHeight >= scrollHeight - 5) {
+        if (cont == 0) {
+            titlePopular.classList.remove('title__popular')
+            factoryPopular(pop, popDiv)
+        } else if (cont == 1) {
+            titleNews.classList.remove('title__news')
+            factoryNews(news, newsDiv)
+        } else if (cont == 2) {
+            titleOpinion.classList.remove('title__opinion')
+            factoryNews(opinion, opinionDiv)
+        } else if (cont == 3) {
+            titleArts.classList.remove('title__arts')
+            factoryNews(arts, artsDiv)
+        } else if (cont == 4) {
+            titleLiving.classList.remove('title__living')
+            factoryNews(living, livingDiv)
+        } else if (cont == 5) {
+            init()
+        } else {
+            window.removeEventListener('scroll', factoryPopular)
+        }
+        cont++
+	}
+});
