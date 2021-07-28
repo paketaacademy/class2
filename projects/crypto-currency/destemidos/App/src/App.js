b5c784b632e5e798a839a338122af9e878aa1f48
Caroline Spezzatto <caroline.spezzatto@gmail.com>
2021-07-28 14:04:06 -0300

    Criando tela de login e cadastro

--- projects/crypto-currency/destemidos/App/src/App.js
+++ projects/crypto-currency/destemidos/App/src/App.js
@@ -1,11 +1,23 @@
+// import React from 'react';
+// import './App.css';
+// // import SignIn from './components/SignIn';
+// // import SignUp from './components/SignUp'
+// import Routes from './components/Routes';
+
+// const App = () => 
+//   <Routes />;
+// export default App;
+
 import React from 'react';
-import { Button } from '@material-ui/core';
-
-
-import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import SignIn from './Pages/SignIn';
 
 function App() {
-  return <Button color="primary">Hello World</Button>;
+  return (
+  <Router>
+    <SignIn/>
+  </Router>
+  );
 }
 
 export default App;
--- projects/crypto-currency/destemidos/App/src/index.js
+++ projects/crypto-currency/destemidos/App/src/index.js
@@ -5,9 +5,6 @@ import App from './App';
 
 
 ReactDOM.render(
-  <>
-    <App />
-  </>,
+    <App />,
   document.getElementById('root')
 );
-
