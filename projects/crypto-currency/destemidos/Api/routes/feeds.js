050c5cdb4b9afb8b62232ee1d95d910b5c4f68a9
Matheus Henrique <mattheus.h.m.s@gmail.com>
2021-07-29 11:04:05 -0300

    feature: routes and mongo

--- projects/crypto-currency/destemidos/Api/routes/feeds.js
+++ projects/crypto-currency/destemidos/Api/routes/feeds.js
@@ -1,21 +1,11 @@
-import app from './app_export.js'
+import { config } from './configs_export/config.js'
+import app from './configs_export/app_export.js'
 import axios from 'axios';
-
-const feeds = () => {
-    return {
-        method: 'GET',
-        url: 'https://api.lunarcrush.com/v2?data=feeds&key=y7ze70q6n18xv9g0jlpjlo',
-        headers: {
-            'Key': 'y7ze70q6n18xv9g0jlpjlo',
-        }
-    }
-};
-
 
 
 app.get('/feeds', function (req, res) {
 
-    axios.request(feeds('feeds')).then(function (response) {
+    axios.request(config('feeds')).then(function (response) {
         let resp = response.data;
         res.json(resp)
     }).catch(function (error) {
