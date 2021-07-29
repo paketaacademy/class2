5ac4dd66659158c7e6ede63c0ce013ec8e448f5f
Leringit <54607457+Leringit@users.noreply.github.com>
2021-07-29 10:48:28 -0300

    Revert "feature:"

--- projects/crypto-currency/iluminados/app/src/components/Login/styles.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-
-export const DivContainerBtn = styled.div`
-  &&{
-    align-self: start;
-  }
-`
--- projects/crypto-currency/iluminados/app/src/components/Register/styles.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components"
-
-export const DivContainerBtn = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-`
