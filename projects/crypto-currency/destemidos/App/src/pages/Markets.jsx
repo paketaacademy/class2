affac4e41756e113d51f5b5c5b2dfbbd1a10ec38
Wanessa Alves <wanessaalves__@hotmail.com>
2021-07-30 00:29:22 -0300

    feature: create market page

---
+++ projects/crypto-currency/destemidos/App/src/pages/Markets.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TableMarket from '../components/TableMarket';
+
+
+function Markets() {
+    return (
+        <>
+            <TableMarket />
+        </>
+    )
+}
+
+export default Markets;
