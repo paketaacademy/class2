1bc947f6e06af27cbefd4b8542a13d87150543ba
Wanessa Alves <wanessaalves__@hotmail.com>
2021-07-31 23:12:07 -0300

    refactor: remove createData and headCells

--- projects/crypto-currency/destemidos/App/src/components/TableMarket/index.jsx
+++ projects/crypto-currency/destemidos/App/src/components/TableMarket/index.jsx
@@ -1,42 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableContainer from '@material-ui/core/TableContainer';
 import TablePagination from '@material-ui/core/TablePagination';
-import Paper from '@material-ui/core/Paper';
+
 import { TableStyle } from './styles';
 import THead from './TableHeader'
 import TBody from './TableBody'
 
-function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
-}
-
-const headCells = [
-    { id: 'name', numeric: false, disablePadding: true, label: 'Dessert (100g serving)' },
-    { id: 'calories', numeric: true, disablePadding: false, label: 'Calories' },
-    { id: 'fat', numeric: true, disablePadding: false, label: 'Fat (g)' },
-    { id: 'carbs', numeric: true, disablePadding: false, label: 'Carbs (g)' },
-    { id: 'protein', numeric: true, disablePadding: false, label: 'Protein (g)' },
-];
-
-
-const rows = [
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Donut', 452, 25.0, 51, 4.9),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Honeycomb', 408, 3.2, 87, 6.5),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Jelly Bean', 375, 0.0, 94, 0.0),
-    createData('KitKat', 518, 26.0, 65, 7.0),
-    createData('Lollipop', 392, 0.2, 98, 0.0),
-    createData('Marshmallow', 318, 0, 81, 2.0),
-    createData('Nougat', 360, 19.0, 9, 37.0),
-    createData('Oreo', 437, 18.0, 63, 4.0),
-];
-
-function TableMarket() {
+function TableMarket(props) {
+    const { rows, headCells } = props;
     const classes = TableStyle();
     const [order, setOrder] = React.useState('asc');
     const [orderBy, setOrderBy] = React.useState('calories');
@@ -117,4 +91,11 @@ function TableMarket() {
         </div>
     );
 }
+
+TableMarket.propTypes = {
+    rows: PropTypes.array.isRequired,
+    headCells: PropTypes.array.isRequired
+};
+
+
 export default TableMarket;
