d1dc9be541156d0913c4c757a0d9d36d5f2ed9f6
Wanessa Alves <wanessaalves__@hotmail.com>
2021-08-01 21:18:55 -0300

    feature: add sticky behaviour

--- projects/crypto-currency/destemidos/App/src/pages/Markets.jsx
+++ projects/crypto-currency/destemidos/App/src/pages/Markets.jsx
@@ -4,35 +4,35 @@ import api from '../api';
 
 
 const headCells = [
-    { id: 'id', numeric: false, disablePadding: true, label: '#' },
-    { id: 'coin', numeric: true, disablePadding: false, label: 'Coin' },
-    { id: 'alerts', numeric: true, disablePadding: false, label: 'Alerts' },
-    { id: 'galaxyScore', numeric: true, disablePadding: false, label: 'Galaxy Score™' },
-    { id: 'altRank', numeric: true, disablePadding: false, label: 'AltRank™' },
-    { id: 'correlationRank', numeric: true, disablePadding: false, label: 'Correlation Rank' },
-    { id: 'price', numeric: true, disablePadding: false, label: 'Price' },
-    { id: '1hChangeUSD', numeric: true, disablePadding: false, label: '1H % Change (USD)' },
-    { id: '24hChangeUSD', numeric: true, disablePadding: false, label: '24H % Change (USD)' },
-    { id: '24hChangeBTC', numeric: true, disablePadding: false, label: '24H % Change (BTC)' },
-    { id: 'marketCap', numeric: true, disablePadding: false, label: 'Market Cap' },
-    { id: 'marketVolume', numeric: true, disablePadding: false, label: 'Market Volume' },
-    { id: 'marketDominance', numeric: true, disablePadding: false, label: 'Market Dominance' },
-    { id: 'volatily', numeric: true, disablePadding: false, label: 'Volatility' },
-    { id: 'circulatingSupply', numeric: true, disablePadding: false, label: 'Circulating Supply' },
-    { id: 'socialVol', numeric: true, disablePadding: false, label: 'Social Volume' },
-    { id: 'socialEng', numeric: true, disablePadding: false, label: 'Social Engagement' },
-    { id: 'socialCont', numeric: true, disablePadding: false, label: 'Social Contributors' },
-    { id: 'socialDom', numeric: true, disablePadding: false, label: 'Social Dominance' },
-    { id: 'averageSent', numeric: true, disablePadding: false, label: 'Average Sentiment' },
-    { id: 'bullishSent', numeric: true, disablePadding: false, label: 'Bullish Sentiment' },
-    { id: 'bearishSent', numeric: true, disablePadding: false, label: 'Bearish Sentiment' },
-    { id: 'sharedLinks', numeric: true, disablePadding: false, label: 'Shared Links' },
-    { id: 'twitterVol', numeric: true, disablePadding: false, label: 'Twitter Volume' },
-    { id: 'redditVol', numeric: true, disablePadding: false, label: 'Reddit Volume' },
-    { id: 'mediumVol', numeric: true, disablePadding: false, label: 'Medium Volume' },
-    { id: 'youtubeVol', numeric: true, disablePadding: false, label: 'Youtube Volume' },
-    { id: 'newsVol', numeric: true, disablePadding: false, label: 'News Volume' },
-    { id: 'spamVol', numeric: true, disablePadding: false, label: 'Spam Volume' },
+    { id: 'id', numeric: false, disablePadding: true, label: '#', sticky: true },
+    { id: 'coin', numeric: false, disablePadding: true, label: 'Coin', sticky: true },
+    { id: 'alerts', numeric: true, disablePadding: false, label: 'Alerts', sticky: false },
+    { id: 'galaxyScore', numeric: true, disablePadding: false, label: 'Galaxy Score™', sticky: false },
+    { id: 'altRank', numeric: true, disablePadding: false, label: 'AltRank™', sticky: false },
+    { id: 'correlationRank', numeric: true, disablePadding: false, label: 'Correlation Rank', sticky: false },
+    { id: 'price', numeric: true, disablePadding: false, label: 'Price', sticky: false },
+    { id: '1hChangeUSD', numeric: true, disablePadding: false, label: '1H % Change (USD)', sticky: false },
+    { id: '24hChangeUSD', numeric: true, disablePadding: false, label: '24H % Change (USD)', sticky: false },
+    { id: '24hChangeBTC', numeric: true, disablePadding: false, label: '24H % Change (BTC)', sticky: false },
+    { id: 'marketCap', numeric: true, disablePadding: false, label: 'Market Cap', sticky: false },
+    { id: 'marketVolume', numeric: true, disablePadding: false, label: 'Market Volume', sticky: false },
+    { id: 'marketDominance', numeric: true, disablePadding: false, label: 'Market Dominance', sticky: false },
+    { id: 'volatily', numeric: true, disablePadding: false, label: 'Volatility', sticky: false },
+    { id: 'circulatingSupply', numeric: true, disablePadding: false, label: 'Circulating Supply', sticky: false },
+    { id: 'socialVol', numeric: true, disablePadding: false, label: 'Social Volume', sticky: false },
+    { id: 'socialEng', numeric: true, disablePadding: false, label: 'Social Engagement', sticky: false },
+    { id: 'socialCont', numeric: true, disablePadding: false, label: 'Social Contributors', sticky: false },
+    { id: 'socialDom', numeric: true, disablePadding: false, label: 'Social Dominance', sticky: false },
+    { id: 'averageSent', numeric: true, disablePadding: false, label: 'Average Sentiment', sticky: false },
+    { id: 'bullishSent', numeric: true, disablePadding: false, label: 'Bullish Sentiment', sticky: false },
+    { id: 'bearishSent', numeric: true, disablePadding: false, label: 'Bearish Sentiment', sticky: false },
+    { id: 'sharedLinks', numeric: true, disablePadding: false, label: 'Shared Links', sticky: false },
+    { id: 'twitterVol', numeric: true, disablePadding: false, label: 'Twitter Volume', sticky: false },
+    { id: 'redditVol', numeric: true, disablePadding: false, label: 'Reddit Volume', sticky: false },
+    { id: 'mediumVol', numeric: true, disablePadding: false, label: 'Medium Volume', sticky: false },
+    { id: 'youtubeVol', numeric: true, disablePadding: false, label: 'Youtube Volume', sticky: false },
+    { id: 'newsVol', numeric: true, disablePadding: false, label: 'News Volume', sticky: false },
+    { id: 'spamVol', numeric: true, disablePadding: false, label: 'Spam Volume', sticky: false },
 ];
 
 function Markets() {
@@ -53,6 +53,7 @@ function Markets() {
             <TableMarket rows={coins} headCells={headCells} />
         </>
     )
+
 }
 
 export default Markets;
