e6e6cd4523cb894cbf2cf2ca62f28f368bfde1a5
Wanessa Alves <wanessaalves__@hotmail.com>
2021-08-01 21:16:48 -0300

    feature: add styles

--- projects/crypto-currency/destemidos/App/src/components/TableMarket/TableBody/index.jsx
+++ projects/crypto-currency/destemidos/App/src/components/TableMarket/TableBody/index.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
-import { getComparator, stableSort } from '../utils'
+import { getComparator, stableSort, getColorOfNumber } from '../utils'
+import { TableCell, StyledP } from '../styles'
+
+
 
 
 function TBody(props) {
     const { order, orderBy, rows, rowsPerPage, page, onClickRow } = props;
 
     return (
-        <TableBody>
+        <TableBody on>
             {stableSort(rows, getComparator(order, orderBy))
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                 .map((row, index) => {
-
-                    const labelId = `enhanced-table-checkbox-${index}`;
 
                     return (
                         <TableRow
@@ -23,20 +23,24 @@ function TBody(props) {
                             onClick={(event) => onClickRow(event, row.name)}
                             role="checkbox"
                             tabIndex={-1}
-                            key={row.name}
-
+                            key={index}
                         >
-                            <TableCell component="th" scope="row" padding="none">
+                            <TableCell sticky component="th" scope="row" padding="none">
                                 {index}
                             </TableCell>
-                            <TableCell align="right">{row.n}:{row.s}</TableCell>
+                            <TableCell sticky borderRight align="right"><div className="t">{row.n}:{row.s}</div></TableCell>
                             <TableCell align="right">+</TableCell>
                             <TableCell align="right">{row.gs}</TableCell>
                             <TableCell align="right">{row.acr}</TableCell>
                             <TableCell align="right">{row.cr}</TableCell>
-                            <TableCell align="right">{row.p} || {row.p_btc}</TableCell>
-                            <TableCell align="right">{row.pch}</TableCell>
-                            <TableCell align="right">{row.pc}</TableCell>
+                            <TableCell align="right">
+                                <p>{row.p}</p>
+                                <StyledP txtColor={getColorOfNumber(row.p_btc)}>
+                                    {row.p_btc.toFixed(9)}
+                                </StyledP>
+                            </TableCell>
+                            <TableCell align="right" txtColor={getColorOfNumber(row.pch)}>{row.pch}</TableCell>
+                            <TableCell align="right" txtColor={getColorOfNumber(row.pc)}>{row.pc}</TableCell>
                             <TableCell align="right">--</TableCell>
                             <TableCell align="right">{row.mc}</TableCell>
                             <TableCell align="right">{row.v}</TableCell>
