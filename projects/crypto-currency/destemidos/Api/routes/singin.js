50d8c3c20785740eb7e90ee3819ef7a1820277d7
Matheus Henrique <mattheus.h.m.s@gmail.com>
2021-08-04 15:25:09 -0300

    Chore: login

--- projects/crypto-currency/destemidos/Api/routes/singin.js
+++ projects/crypto-currency/destemidos/Api/routes/singin.js
@@ -1,7 +1,26 @@
 import app from "./configs/app.js"
+import { Mongoose, UserSchema } from './configs/db.js'
 
-app.get('/login', (req,res)=>{
-    res.send('Fazer o login')
+app.post('/singin', async (req, res) => {
+ 
+  const email = req.body.email
+  const password = req.body.password
+  
+  const Users = Mongoose.model('users', UserSchema, 'users')
+  
+  try {
+    const foundUser = await Users.findOne({ email: email, password: password }).exec()
+    
+      if(foundUser) {
+        res.status(200).send('Login realizado com sucesso')
+        
+      } else {
+          res.status(401).send('Login invalido')
+      }
+
+  } catch (err) {
+    res.send(err)
+  }
 })
 
 export default app
