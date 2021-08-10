1c33a69e34a0113d88e664cc230aa3bad61bc62a
renatosheep <renato.carneirofs@gmail.com>
2021-08-10 09:45:22 -0300

    feature
    O que foi feito?
    
    -Criação de rota de inserção de crédito pelo cliente e possíveis validações destas entradas.
    
    O que falta?
    
    -Realizar a validação da compra realizada pelo cliente e terminar a rota de compra.

---
+++ projects/crypto-currency/iluminados/api/src/routes/modules/bank.js
@@ -0,0 +1,43 @@
+import express from "express"
+import { bankValidation } from "./validation.js"
+import { Mongoose, bankSchema } from "./configs/data-base.js"
+import verifyToken from "./configs/verify-token.js"
+
+const app = express()
+app.use(express.json())
+
+app.post("/bank", verifyToken, async (req, res) => {
+  const { error } = bankValidation(req.body)
+  if (error) {
+    return res
+    .status(400)
+    .send(error.details[0].message)
+  }
+  
+  try {
+    const { balance } = req.body
+    const bankModel = Mongoose.model("bank", bankSchema, "bank")
+  
+    const user = req.user._id
+    
+    if(user) {
+      return res
+          .status(400)
+          .send({
+            error:'Account bank already exists.'
+          })
+    }
+    await bankModel.create({
+      user,
+      balance,
+    })
+    return res.status(201).send("Saldo inserido")
+    
+  } catch (error) {
+    return res.status(400).send({
+      error: `Ocorreu um erro - ${error}`,
+    })
+  }
+});
+
+export default app
--- projects/crypto-currency/iluminados/api/src/routes/modules/validation.js
+++ projects/crypto-currency/iluminados/api/src/routes/modules/validation.js
@@ -1,23 +1,42 @@
-import Joi from '@hapi/joi'
+import Joi from "@hapi/joi";
 
 const registerValidation = (data) => {
-    const schema = Joi.object({
-        firstName: Joi.string().min(3).required(),
-        lastName: Joi.string().min(3).required(),
-        email: Joi.string().min(6).required().email().lowercase(),
-        password: Joi.string().min(6).required()
-      })
-      return schema.validate(data)
-}
+  const schema = Joi.object({
+    firstName: Joi.string().min(3).required(),
+    lastName: Joi.string().min(3).required(),
+    email: Joi.string().min(6).required().email().lowercase(),
+    password: Joi.string().min(6).required(),
+  });
+  return schema.validate(data);
+};
 
 const loginValidation = (data) => {
-    const schema = Joi.object({
-        firstName: Joi.string().min(3).required(),
-        lastName: Joi.string().min(3).required(),
-        email: Joi.string().min(6).required().email().lowercase(),
-        password: Joi.string().min(6).required()
-      })
-      return schema.validate(data)
-}
+  const schema = Joi.object({
+    email: Joi.string().min(6).required().email().lowercase(),
+    password: Joi.string().min(6).required(),
+  });
+  return schema.validate(data);
+};
 
-export { registerValidation, loginValidation }
+const walletValidation = (data) => {
+  const schema = Joi.object({
+    coinName: Joi.string().min(1).required().lowercase(),
+    coinPrice: Joi.string().min(1).required(),
+    coinQuantity: Joi.string().min(1).required(),
+  });
+  return schema.validate(data);
+};
+
+const bankValidation = (data) => {
+  const schema = Joi.object({
+    balance: Joi.number().min(1).required(),
+  });
+  return schema.validate(data);
+};
+
+export {
+  registerValidation,
+  loginValidation,
+  walletValidation,
+  bankValidation,
+};
