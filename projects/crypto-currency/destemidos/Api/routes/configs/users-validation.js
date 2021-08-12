fdf07004b2b35cd9f2ddae884432fdc266e5ea4d
Matheus Henrique <mattheus.h.m.s@gmail.com>
2021-08-12 16:31:35 -0300

    melhoria na validação de email

--- projects/crypto-currency/destemidos/Api/routes/configs/users-validation.js
+++ projects/crypto-currency/destemidos/Api/routes/configs/users-validation.js
@@ -2,7 +2,7 @@ import Joi from 'joi'
 
 const singinValidation = (data) => {
   const schema = Joi.object({
-    email: Joi.string().min(9).required(),
+    email: Joi.string().min(9).required().email,
     password: Joi.string().min(6).required()
   })
   return schema.validate(data)
@@ -10,7 +10,7 @@ const singinValidation = (data) => {
 
 const singupValidation = (data) => {
   const schema = Joi.object({
-    email: Joi.string().min(9).required(),
+    email: Joi.string().min(9).required().email(),
     password: Joi.string().min(6).required(),
     confirmPassword: Joi.ref('password'),
   })
