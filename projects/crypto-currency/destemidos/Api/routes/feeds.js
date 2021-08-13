76f87d8b630957a42c3065649f6139cd59a7b744
Rafael Jesus do Prado <rafaeljprado@gmail.com>
2021-08-13 16:04:55 -0300

    Chore: alterações final

--- projects/crypto-currency/destemidos/Api/routes/feeds.js
+++ projects/crypto-currency/destemidos/Api/routes/feeds.js
@@ -1,12 +1,11 @@
-import { config } from './configs/api-lunarcrush.js'
 import app from './configs/app.js'
+import { config2 } from './configs/api-lunarcrush2.js'
 import axios from 'axios'
 
-app.get('/feeds', function (req, res) {
-
-  axios.request(config('feeds')).then(function (response) {
+app.get('/feeds', function(req, res){
+  axios.request(config2('feeds')).then(function (response) {
     let resp = response.data
-      res.json(resp)
+    res.json(resp)
     }).catch(function (error) {
       console.error(error)
     })
