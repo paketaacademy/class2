d06b04a089fcb1d5538ff682df08a5a1a648622e
Alisson Perim Honostorio <alissonhonostorio@gmail.com>
2021-08-17 14:53:19 -0300

    feature: cria board
    cria board e board schema

--- projects/generic-trello/batman/api/src/routes/index.js
+++ projects/generic-trello/batman/api/src/routes/index.js
@@ -1,8 +1,11 @@
-import { Router } from "express";
+import { Router } from "express"
+import { BoardController } from "../controllers/Boards/post.js"
+import verifyToken from "../middlewares/verify-token.js"
 
 const routes = Router()
-routes.get('/', (req, res)=>{
+routes.get('/', (req, res) => {
     res.send('funcionou')
 })
+routes.post("/create-board", verifyToken, BoardController.createBoard)
 
 export default routes
