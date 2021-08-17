1ad7b9c2241b4c8068b02691d637ed1bee2a9eb2
renatosheep <renato.carneirofs@gmail.com>
2021-08-17 14:12:25 -0300

    Update validation.js
    adicionado requisitos de validação em email e senha

--- projects/generic-trello/batman/api/src/config/validation.js
+++ projects/generic-trello/batman/api/src/config/validation.js
@@ -4,8 +4,8 @@ const registerValidation = (data) => {
   const schema = yup.object().shape({
     nome: yup.string().required(),
     sobrenome: yup.string().required(), 
-    email: yup.string().min(6).email(),
-    senha: yup.string().min(6),
+    email: yup.string().min(6).email().required(),
+    senha: yup.string().min(6).required(),
     })
     return schema.validate(data)
 }
