fb4adc99fb091c95552e2f06f29a2669b6ef2352
Matheus Henrique <mattheus.h.m.s@gmail.com>
2021-08-17 15:36:24 -0300

    feature: login

---
+++ projects/generic-trello/batman/api/src/controllers/Users/login-post.js
@@ -0,0 +1,54 @@
+import userSchema from "../../models/user-schema.js"
+import { Mongoose } from "../../index.js"
+import bcrypt from 'bcrypt'
+import dotenv from 'dotenv'
+import jwt from "jsonwebtoken"
+import { loginValidation } from '../../config/validation.js'
+
+dotenv.config()
+
+const UserLogin = {
+
+    async loginUser (req, res) {
+        const { error } = loginValidation(req.body)
+        if (error) {
+          return res
+          .status(400)
+          .send(error.details[0].message)
+        }
+       
+        const{ email, password } = req.body
+        const userBd = Mongoose.model('users', userSchema, 'users')
+      
+        const user = await userBd.findOne({ email })
+        if(!user){
+          return res
+          .status(400)
+          .send({
+              menssagem:'email não encontrado'
+            })
+        }
+      
+        const validPass = await bcrypt.compare(password, user.password)
+      
+        if(!validPass){
+          return res
+          .status(400)
+          .send({
+              menssagem:'Senha invalida'
+            })
+        }
+      
+        const token = jwt.sign({_id: user._id}, process.env.SECRET, { 
+          expiresIn: 2592000
+      })
+        res.
+        header('auth-token', token)
+        res.
+        send({
+            menssagem:'Login realizado com sucesso!'
+        })
+      }
+}
+
+export default UserLogin
--- projects/generic-trello/batman/api/src/routes/index.js
+++ projects/generic-trello/batman/api/src/routes/index.js
@@ -1,7 +1,10 @@
 import { Router } from "express"
-import UserRegister from "../controllers/Users/post.js"
+import UserRegister from "../controllers/Users/register-post.js"
+import UserLogin from "../controllers/Users/login-post.js"
+import verifyToken from "../middlewares/verify-token.js"
 
 const routes = Router()
 routes.post('/register', UserRegister.creatUser)
+routes.post('/login', UserLogin.loginUser)
 
 export default routes
