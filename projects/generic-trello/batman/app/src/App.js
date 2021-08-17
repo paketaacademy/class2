cc11d8baf0a8ff332dc38897698af287541c4476
Caroline Spezzatto <caroline.spezzatto@gmail.com>
2021-08-17 15:58:03 -0300

    Excluindo o que não foi utilizado

--- projects/generic-trello/batman/app/src/App.js
+++ projects/generic-trello/batman/app/src/App.js
@@ -1,20 +1,13 @@
 import React from "react"
 import Home from './components/Home'
 import { BrowserRouter as Router } from "react-router-dom"
-import { createTheme, StylesProvider } from "@material-ui/core/styles"
-import { ThemeProvider } from "styled-components"
 
 function App() {
 
-  const theme = createTheme()
   return (
-    <StylesProvider injectFirst>
-      <ThemeProvider theme={theme}>
-        <Router>
-          <Home />
-        </Router>
-      </ThemeProvider>
-    </StylesProvider>
+    <Router>
+      <Home />
+    </Router>
   )
 }
 
