238942611c51bb972de436586d1351db5cbc587f
Alisson Perim Honostorio <alissonhonostorio@gmail.com>
2021-08-17 16:32:40 -0300

    feature: cria novas rotas para o board
    foram criadas as rotas de patch, delete, melhorado o schema e modificado a rota post

--- projects/generic-trello/batman/api/src/routes/index.js
+++ projects/generic-trello/batman/api/src/routes/index.js
@@ -1,10 +1,15 @@
 import { Router } from "express"
-import { BoardController } from "../controllers/Boards/post.js"
 import verifyToken from "../middlewares/verify-token.js"
 import UserRegister from "../controllers/Users/post.js"
+import { BoardControllerPost } from "../controllers/Boards/post.js"
+import { BoardControllerUpdate } from "../controllers/Boards/patch.js"
+import { BoardControllerDelete } from "../controllers/Boards/delete.js"
 
 const routes = Router()
 routes.post('/register', verifyToken, UserRegister.creatUser)
-routes.post("/create-board", verifyToken, BoardController.createBoard)
+
+routes.post("/create-board", verifyToken, BoardControllerPost.CreateBoard)
+routes.patch("/update-board", verifyToken, BoardControllerUpdate.UpdateBoard)
+routes.delete("/delete-board", verifyToken, BoardControllerDelete.DeleteBoard)
 
 export default routes
