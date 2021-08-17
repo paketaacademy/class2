cc45cd1e461d5a20038e48e814757ac4a01687b9
renatosheep <renato.carneirofs@gmail.com>
2021-08-17 13:23:14 -0300

    feature: verifyToken
    criação da função para validar o JWT

--- projects/generic-trello/batman/api/src/index.js
+++ projects/generic-trello/batman/api/src/index.js
@@ -1,13 +1,28 @@
 import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
+import mongoose from 'mongoose'
+import routes from './routes/index.js'
+dotenv.config()
+
 const app = express()
+app.use(express.json())
+app.use(routes)
+
 const corsOpition = {
   exposedHeaders: 'auth-token'
 }
+app.use(cors(corsOpition))
 
 dotenv.config()
-app.use(express.json())
-app.use(cors(corsOpition))
+
+const params = {
+    'useNewUrlParser': true,
+    'useUnifiedTopology': true
+}
+const { MONGO_URI } = process.env
+mongoose.connect(MONGO_URI, params, () => console.log ('Connect to dataBase'))
+
+export { mongoose as Mongoose }
 
 export default app
