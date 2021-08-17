a1772c5ef188c2adf2fecdd886d53b02035d33a3
Caroline Spezzatto <caroline.spezzatto@gmail.com>
2021-08-17 15:46:19 -0300

    Feature: Criando tela Cadastro de usuário

--- projects/generic-trello/batman/app/src/App.js
+++ projects/generic-trello/batman/app/src/App.js
@@ -1,12 +1,20 @@
 import React from "react"
 import Home from './components/Home'
 import { BrowserRouter as Router } from "react-router-dom"
+import { createTheme, StylesProvider } from "@material-ui/core/styles"
+import { ThemeProvider } from "styled-components"
 
 function App() {
+
+  const theme = createTheme()
   return (
-    <Router>
-      <Home />
-    </Router>
+    <StylesProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Home />
+        </Router>
+      </ThemeProvider>
+    </StylesProvider>
   )
 }
 
