2cc2fdfe87c11aae61eef36a69e3f1ca12693a3f
Lucas Barbosa Dias <lucasbarbosadias@outlook.com>
2021-08-18 14:02:29 -0300

    bugfix: front-end with back-end integration

--- projects/generic-trello/superman/api/routes/configs/user-validation.js
+++ projects/generic-trello/superman/api/routes/configs/user-validation.js
@@ -1,9 +1,9 @@
 import * as yup from 'yup'
   
   export const userSchema = yup.object().shape({
-    name: yup.string().trim().min(9, 'Quantidade de caracteres deve ser maior que 9').required(),
-    email: yup.string().trim().min(9, 'Quantidade de caracteres deve ser maior que 9').email().required(),
-    password: yup.string().min(6, 'Quantidade de caracteres deve ser maior que 6').required(),
+    name: yup.string().trim().min(9, 'Nome deve possuir mais de 9 caracteres').required(),
+    email: yup.string().trim().min(9, 'Email deve possuir mais de 9 caracteres').email('Digite um email válido!').required(),
+    password: yup.string().min(6, 'Senha deve possuir mais de 6 caracteres').required(),
   })
 
 
