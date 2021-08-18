eae7a4555a0b49aaf2747e23531c623762a8cd92
Rafael Jesus do Prado <rafaeljprado@gmail.com>
2021-08-17 21:39:15 -0300

    chore: alterado contentPage, rotas com caminhos corretos sem comentários

--- projects/generic-trello/superman/app/src/components/ContentPage/index.jsx
+++ projects/generic-trello/superman/app/src/components/ContentPage/index.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
+import Login from '../../pages/Login'
+import Board from '../../pages/Board'
+import Profile from '../../pages/Profile'
+import SingUp from '../../pages/SingUp'
 
 function ContentPage() {
   return (
     <Switch>
-      {/* <Route exact path='/'>
-        <SignIn />
+      <Route exact path='/'>
+        <Login />
+      </Route>
+      <Route exact path='/cadastrar'>
+        <SingUp />
       </Route>
       <Route exact path='/perfil'>
         <Profile />
       </Route>
       <Route exact path='/quadro'>
         <Board />
-      </Route> */}
+      </Route>
     </Switch>
   )
 }
