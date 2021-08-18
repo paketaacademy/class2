df600ed0cbdeb7b41fd870751eda79665df1f541
renatosheep <renato.carneirofs@gmail.com>
2021-08-18 10:18:45 -0300

    feature
    O que foi feito?
    
    criação de middleware para validação das informações cadastrada pelo usuário.
    
    Como foi feito?
    
    *Criação da pasta validation que contempla os arquivos relacionados ao YUP.
    
    *Na pasta middlewares, foram adicionados arquivos que contêm funções que irão validar as informações digitadas pelo usuário.

--- projects/generic-trello/batman/api/src/routes/index.js
+++ projects/generic-trello/batman/api/src/routes/index.js
@@ -3,10 +3,17 @@ import { BoardController } from "../controllers/Boards/post.js"
 import UserRegister from "../controllers/Users/register-post.js"
 import UserLogin from "../controllers/Users/login-post.js"
 import verifyToken from "../middlewares/verify-token.js"
+import { validateUser } from "../middlewares/user-validate.js"
+import { UserSchema } from "../validation/user-validation.js"
+import { validateLogin } from "../middlewares/login-validate.js"
+import { LoginSchema } from "../validation/login-validation.js"
+import { validateBoard } from "../middlewares/board-validate.js"
+import { BoardSchema } from "../validation/board-validation.js"
+
 
 const routes = Router()
-routes.post('/register', UserRegister.creatUser)
-routes.post('/login', UserLogin.loginUser)
-routes.post("/create-board", verifyToken, BoardController.createBoard)
+routes.post('/register', validateUser(UserSchema), UserRegister.creatUser)
+routes.post('/login', validateLogin(LoginSchema), UserLogin.loginUser)
+routes.post("/create-board", validateBoard(BoardSchema), verifyToken, BoardController.createBoard)
 
 export default routes
