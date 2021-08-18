444efad648b49e0cb6fa0e53d5e9cdd662ea48eb
LilianCarvalho <liliancarol93@gmail.com>
2021-08-18 10:44:22 -0300

    Criando tela de login

--- projects/generic-trello/batman/app/src/pages/Login/index.js
+++ projects/generic-trello/batman/app/src/pages/Login/index.js
@@ -1,11 +1,89 @@
 import React from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import TextField from '@material-ui/core/TextField'
+import Link from '@material-ui/core/Link'
+import Grid from '@material-ui/core/Grid'
+import Box from '@material-ui/core/Box'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
+import { PaperDiv, StyledAvatar, Form, SubmitButton } from './styles'
+import './styles'
 
-function login() {
-  return(
-    <div>
-      <h1>Login</h1>
-    </div>
-  )
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-export default login
+export default function SignUp() {
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <PaperDiv>
+        <StyledAvatar>
+          <LockOutlinedIcon />
+        </StyledAvatar>
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
+        <Form noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Senha"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+
+          <SubmitButton
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={SubmitButton}
+          >
+            Fazer Login
+          </SubmitButton>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Esqueceu sua senha?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Não possui cadastro? Cadastrar-se"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Form>
+      </PaperDiv>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
