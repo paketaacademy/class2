d9d808c5f7e401537c7b6a723036c69073e71e61
Rafael Jesus do Prado <rafaeljprado@gmail.com>
2021-08-18 16:43:55 -0300

    chore: acrescentado validação do usuário ao fazer login e proteção nas paginas com autenticação

--- projects/generic-trello/superman/api/index.js
+++ projects/generic-trello/superman/api/index.js
@@ -1,12 +1,21 @@
 import home from './routes/home.js'
 import register from './routes/register.js'
+import login from './routes/login.js'
 import express from 'express'
+import cors from 'cors'
+
 const app = express()
 
 const { LISTEN_DOOR } = process.env
 
+const corsOptions = {
+  exposedHeaders: 'auth-superman'
+}
+
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(home)
 app.use(register)
+app.use(login)
 
 app.listen(LISTEN_DOOR)
