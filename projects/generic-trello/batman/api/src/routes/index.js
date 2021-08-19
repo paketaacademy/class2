b5bb085196022f8b7b1cdbc207e8b25af78b802b
Alisson Perim Honostorio <alissonhonostorio@gmail.com>
2021-08-19 18:40:14 -0300

    feature: cria rota assign-patch
    Foi criado a rota assign-patch, para assinar o usuário no board, só é feito a adição de 1 usuário por vez, também foi criado a rota no arquivo index e a validação nos arquivos board-validate e board-validation

--- projects/generic-trello/batman/api/src/routes/index.js
+++ projects/generic-trello/batman/api/src/routes/index.js
@@ -7,13 +7,14 @@ import { validateUser } from "../middlewares/user-validate.js"
 import { UserSchema } from "../validation/user-validation.js"
 import { validateLogin } from "../middlewares/login-validate.js"
 import { LoginSchema } from "../validation/login-validation.js"
-import { validateBoard } from "../middlewares/board-validate.js"
-import { BoardSchema, BoardUpdateSchema } from "../validation/board-validation.js"
+import { validateAssign, validateBoard } from "../middlewares/board-validate.js"
+import { BoardAssignSchema, BoardSchema, BoardUpdateSchema } from "../validation/board-validation.js"
 import { BoardControllerPost } from "../controllers/Boards/post.js"
 import { BoardControllerUpdate } from "../controllers/Boards/patch.js"
 import { BoardControllerDelete } from "../controllers/Boards/delete.js"
 import deleteListColunm from "../controllers/lists/delete.js"
 import ChangeLists from "../controllers/lists/patch.js"
+import { BoardAssignControllerPatch } from "../controllers/Boards/assign-patch.js"
 
 const routes = Router()
 routes.patch('/list/:id', verifyToken, ChangeLists.UpdateList)
@@ -24,6 +25,7 @@ routes.post('/login', validateLogin(LoginSchema), UserLogin.loginUser)
 
 routes.post("/board", verifyToken, validateBoard(BoardSchema), BoardControllerPost.CreateBoard)
 routes.patch("/board/:id", verifyToken, validateBoard(BoardUpdateSchema), BoardControllerUpdate.UpdateBoard)
+routes.patch("/board/:id/:email", verifyToken, validateAssign(BoardAssignSchema), BoardAssignControllerPatch.BoardAssign)
 routes.delete("/board/:id", verifyToken, BoardControllerDelete.DeleteBoard)
 
 
