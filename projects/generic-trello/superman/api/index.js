95f2ae84c55f46f90571eac1ce8599d731afd204
Lucas Barbosa Dias <lucasbarbosadias@outlook.com>
2021-08-19 14:55:36 -0300

    feature: writing list in the bank

--- projects/generic-trello/superman/api/index.js
+++ projects/generic-trello/superman/api/index.js
@@ -1,7 +1,9 @@
-import home from './routes/home.js'
-import profile from './routes/profile.js'
 import register from './routes/register.js'
 import login from './routes/login.js'
+import profile from './routes/profile.js'
+import home from './routes/home.js'
+import list from './routes/list.js'
+
 import express from 'express'
 import cors from 'cors'
 
@@ -15,10 +17,11 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 app.use(express.json())
-app.use(home)
-app.use(profile)
+
 app.use(register)
 app.use(login)
-
+app.use(profile)
+app.use(home)
+app.use(list)
 
 app.listen(LISTEN_DOOR)
