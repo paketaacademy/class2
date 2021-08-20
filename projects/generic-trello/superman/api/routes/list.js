29e64450667322f61a27b6ec909e60748aa053b1
Douglas Bernardo <douglasbernardo01@hotmail.com>
2021-08-20 12:03:41 -0300

    feature: Criando rota delete do list

--- projects/generic-trello/superman/api/routes/list.js
+++ projects/generic-trello/superman/api/routes/list.js
@@ -1,35 +1,22 @@
 import app from "./configs/app.js"
-import { Mongoose,ListsSchema, BoardsSchema} from './configs/mongo.js'
+import { Mongoose, ListsSchema } from './configs/mongo.js'
 
-app.delete("/list",async (req, res) => {
+app.delete("/list", async (req, res) => {
   const { idList, idBoard } = req.body
 
   const Lists = Mongoose.model('lists', ListsSchema, 'lists')
-  const Boards = Mongoose.model('boards', BoardsSchema, 'boards')
 
-  const foundBoard = await Boards.findOne({ id: idBoard })
-  console.log("inicio")
+  const foundLists = await Lists.findOne({ _id: idList })
 
-  if(foundBoard){
+  if (foundLists && foundLists.idBoard == idBoard) {
+    Lists.deleteOne({ _id: idList }).exec()
 
-  console.log("entrou")
-
-    Lists.deleteOne({ id: idList }, (err) => {
-
-        if(err) return res.status(400).json({
-            error: true,
-            message: "Erro lista não encontrada"
-        })
-
-        return res.json({
-            error: false,
-            message: "Lista apagada com sucesso!"
-        })
+    return res.status(200).json({
+      error: false,
+      message: "Lista apagada com sucesso!"
     })
   }
-  console.log("final")
-
-  return res.status(404).send('Quadro não encontrado')
+  return res.status(404).send('Lista ou Quadro não encontrados')
 })
 
 export default app
