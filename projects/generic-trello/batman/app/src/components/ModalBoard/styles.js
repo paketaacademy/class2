760774b58ede376a9a162f52a560105c9f34733f
Alisson Perim Honostorio <alissonhonostorio@gmail.com>
2021-08-29 15:33:37 -0300

    feature: criado modal para criar board
    foi criado um component novo chamado ModalBoard que é responsável por criar um novo modal, assim que o usuário registra os dados exigidos e clica em salvar, o modal é fechado e a tela atualizada com o novo board presente. Foi usado uma biblioteca chamada react-modal e styled component

---
+++ projects/generic-trello/batman/app/src/components/ModalBoard/styles.js
@@ -0,0 +1,53 @@
+import styled from "styled-components"
+import { Button, TextField } from "@material-ui/core"
+import Modal from "react-modal"
+
+export const AddBoard = styled(Button)`
+  background-color: ${(props) => props.bgcolor || "rgb(255, 235, 80)"};
+  color: ${(props) => props.fontcolor || "rgb(0, 0, 0)"};
+  &:hover{
+    background-color: ${(props) => props.hovercolor || "rgb(255, 235, 00)"}
+  }
+`
+
+export const StyledModal = styled(Modal)`
+  position: absolute;
+  inset: 28%;
+  border: 1px solid rgb(204, 204, 204);
+  background-color: rgb(255, 235, 80);
+  overflow: auto;
+  border-radius: 4px;
+  outline: none;
+  padding: 20px;
+  
+  .actions-buttons{
+    display: flex;
+    margin: 24px 0;
+    .cancel-button-container{
+      margin: 0 0 0 24px;
+    }
+  }
+`
+
+export const Form = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  .MuiInputBase-input{
+    background-color: rgb(255, 255, 255);
+  }
+  .Mui-focused{
+    color: rgb(0, 0, 0);
+  }
+  .Mui-focused .MuiOutlinedInput-notchedOutline{
+    border-color: rgb(0, 0, 0);
+  }
+  .MuiOutlinedInput-multiline.MuiOutlinedInput-marginDense{
+    background-color: rgb(255, 255, 255);
+  }
+`
+
+export const TxtField = styled(TextField)`
+  margin: 16px 0;
+
+`
