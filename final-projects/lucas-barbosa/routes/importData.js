e3b4a3324aef31c835651570f3c1d2787bb66350
Lucas Barbosa Dias <lucasbarbosadias@outlook.com>
2021-09-09 09:58:44 -0300

    feature: converting date and salaries to the american standard

--- final-projects/lucas-barbosa/routes/importData.js
+++ final-projects/lucas-barbosa/routes/importData.js
@@ -9,8 +9,22 @@ app.post('/import', async (req, res) => {
   const sheetNameList = excel.SheetNames
   const dataJson = XLSX.utils.sheet_to_json(excel.Sheets[sheetNameList[0]]);
 
-  return res.send(dataJson)
+  const dataJsonProcessed = []
+  for(let i = 0; i < dataJson.length; i++){
+    const data = dataJson[i]
 
+    const date = data.HiredAt[2] == '/' ? data.HiredAt.split('/').reverse().join('-') : data.HiredAt.split('/').join('-')
+    const netSalary = data.NetSalary.toLocaleString('en-US', { style: 'currency', currency: 'USD' })
+    const grossSalary = data.GrossSalary.toLocaleString('en-US', { style: 'currency', currency: 'USD' })
+    dataJsonProcessed.push({
+      ...data,
+      HiredAt: date,
+      NetSalary: netSalary,
+      GrossSalary: grossSalary
+    })
+  }
+
+  return res.send(dataJsonProcessed)
 })
 
 export default app
