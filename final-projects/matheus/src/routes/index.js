ab6dd666f7fda95f6c5a7bc53b481a3428fba9b1
Matheus Henrique <mattheus.h.m.s@gmail.com>
2021-09-14 11:43:18 -0300

    feature: swagger
    inicio do trabalho com o swager

--- final-projects/matheus/src/routes/index.js
+++ final-projects/matheus/src/routes/index.js
@@ -7,34 +7,25 @@ import Register from "../controllers/users/register.js"
 import LoginValidation from "../validations/login.js"
 import Login from "../controllers/users/login.js"
 import verifyToken from "../middlewares/verify-token.js"
-import Weapon from "../controllers/equipaments/weapon/post.js"
-import Armor from "../controllers/equipaments/armor/post.js"
-import SwordMaster from "../controllers/army/sword-master/post.js"
-import Knight from "../controllers/army/knight/post.js"
-import Villager from "../controllers/army/villager/post.js"
-import Archer from "../controllers/army/archer/post.js"
-import VillagerDel from "../controllers/army/villager/delete.js"
-import SMDel from "../controllers/army/sword-master/delete.js"
-import KnightDel from "../controllers/army/knight/delete.js"
-import ArcherDel from "../controllers/army/archer/delete.js"
-import ModifyArcher from "../controllers/army/archer/patch.js"
-import ModifyKnight from "../controllers/army/knight/patch.js"
-import ModifySM from "../controllers/army/sword-master/patch.js"
-import ModifyVillager from "../controllers/army/villager/patch.js"
-import GetArcher from "../controllers/army/archer/get.js"
-import GetKnight from "../controllers/army/knight/get.js"
-import GetSM from "../controllers/army/sword-master/get.js"
-import GetVillager from "../controllers/army/villager/get.js"
-import ModifyArmor from "../controllers/equipaments/armor/patch.js"
-import DeleteArmor from "../controllers/equipaments/armor/delete.js"
-import GetArmor from "../controllers/equipaments/armor/get.js"
-import ModifyWeapon from "../controllers/equipaments/weapon/patch.js"
-import DeleteWeapon from "../controllers/equipaments/weapon/delete.js"
-import GetWeapon from "../controllers/equipaments/weapon/get.js"
-import GetArmy from "../controllers/users/army.js"
+
 import GetSiegeWeapon from "../controllers/equipaments/siege-weapons.js/get.js"
 import SiegeWeapon from "../controllers/equipaments/siege-weapons.js/post.js"
 import DeleteSiegeWeapon from "../controllers/equipaments/siege-weapons.js/delete.js"
+import GetEquipaments from "../controllers/equipaments/general-equipaments/get.js"
+import Equipaments from "../controllers/equipaments/general-equipaments/post.js"
+import ModifyEquipaments from "../controllers/equipaments/general-equipaments/patch.js"
+import DeleteEquipaments from "../controllers/equipaments/general-equipaments/delete.js"
+import ModifySiegeWeapon from "../controllers/equipaments/siege-weapons.js/patch.js"
+import Soldier from "../controllers/soldier/post.js"
+import DeleteSoldier from "../controllers/soldier/delete.js"
+import GetSoldier from "../controllers/soldier/get.js"
+import ModifySoldier from "../controllers/soldier/patch.js"
+import swaggerUi from 'swagger-ui-express'
+import { createRequire } from "module"
+import BattleRegister from "../controllers/battle/battle-register.js"
+import SetWar from "../controllers/battle/war.js"
+const require = createRequire(import.meta.url)
+const swaggerFile = require("../swagger-output.json")
 
 
 const routes = Router()
@@ -45,53 +36,35 @@ const corsOpition = {
 
 routes.use(cors(corsOpition))
 
+routes.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
 routes.post('/register', ValidateUser(RegisterValidation), Register.UserRegister)
 routes.post('/login', ValidateUser(LoginValidation), Login.UserLogin)
-routes.get('/army', verifyToken, GetArmy.Army)
 
+routes.post('/soldiers', verifyToken, Soldier.CreateSoldier)
+routes.delete('/soldiers', verifyToken, DeleteSoldier.DelSoldier)
+routes.get('/soldiers', verifyToken, GetSoldier.GetOneSoldier)
+routes.get('/all-soldiers', verifyToken, GetSoldier.GetAllSoldiers)
+routes.get('/user/soldiers', verifyToken, GetSoldier.GetByOwnerSoldiers)
+routes.get('/category/soldiers', verifyToken, GetSoldier.GetCategorySoldiers)
+routes.patch('/soldiers', verifyToken, ModifySoldier.ModSoldier)
+
+routes.post('/war', verifyToken, SetWar.War)
+routes.get('/battle-registers', verifyToken, BattleRegister.Register)
+
+routes.get('/user/siege-weapons', verifyToken, GetSiegeWeapon.GetByOwnerSiegeWeapon)
 routes.get('/all-siege-weapons', verifyToken, GetSiegeWeapon.GetAllSiegeWeapon)
 routes.get('/siege-weapons', verifyToken, GetSiegeWeapon.GetOneSiegeWeapon)
 routes.post('/siege-weapons', verifyToken, SiegeWeapon.CreateSiegeWeapon)
 routes.delete('/siege-weapons', verifyToken, DeleteSiegeWeapon.DelSiegeWeapon)
-routes.patch('/siege-weapons', verifyToken, ModifyWeapon.ModWeapon)
+routes.patch('/siege-weapons', verifyToken, ModifySiegeWeapon.ModSiegeWeapon)
 
-
-routes.post('/weapons', verifyToken, Weapon.CreateWeapon)
-routes.patch('/weapons', verifyToken, ModifyWeapon.ModWeapon)
-routes.delete('/weapons', verifyToken, DeleteWeapon.DelWeapon )
-routes.get('/weapons', verifyToken, GetWeapon.GetOneWeapon)
-routes.get('/all-weapons', verifyToken, GetWeapon.GetAllWeapon)
-
-routes.post('/armors', verifyToken, Armor.CreateArmor)
-routes.patch('/armors', verifyToken, ModifyArmor.ModArmor)
-routes.delete('/armors', verifyToken, DeleteArmor.DelArmor)
-routes.get('/armors', verifyToken, GetArmor.GetOneArmor)
-routes.get('/all-armors', verifyToken, GetArmor.GetAllArmor)
-
-routes.delete('/sword-masters', verifyToken, SMDel.SMDelete)
-routes.post('/sword-masters', verifyToken, SwordMaster.CreateSwordMaster)
-routes.patch('/sword-masters', verifyToken, ModifySM.ModSM)
-routes.get('/sword-masters', verifyToken, GetSM.GetOneSM)
-routes.get('/all-sword-masters', verifyToken, GetSM.GetAllSM)
-
-routes.delete('/knights', verifyToken, KnightDel.KinghtDelete)
-routes.post('/knights', verifyToken, Knight.CreateKnight)
-routes.patch('/knights', verifyToken, ModifyKnight.ModKnight)
-routes.get('/knights', verifyToken, GetKnight.GetOneKnight)
-routes.get('/all-knights', verifyToken, GetKnight.GetAllKnight)
-
-routes.delete('/villagers', verifyToken, VillagerDel.VillagerDelete)
-routes.post('/villagers', verifyToken, Villager.CreateVillager)
-routes.patch('/villagers', verifyToken, ModifyVillager.ModVillager)
-routes.get('/villagers', verifyToken, GetVillager.GetOneVillager)
-routes.get('/all-villagers', verifyToken, GetVillager.GetAllVillager)
-
-routes.delete('/archers', verifyToken, ArcherDel.ArcherDelete)
-routes.post('/archers', verifyToken, Archer.CreateArcher)
-routes.patch('/archers', verifyToken, ModifyArcher.ModArcher)
-routes.get('/archers', verifyToken, GetArcher.GetOneArcher)
-routes.get('/all-archers', verifyToken, GetArcher.GetAllArchers)
-
+routes.post('/equipaments', verifyToken, Equipaments.CreateEquipaments)
+routes.patch('/equipaments', verifyToken, ModifyEquipaments.ModEquipaments)
+routes.delete('/equipaments', verifyToken, DeleteEquipaments.DelEquipaments)
+routes.get('/equipaments', verifyToken, GetEquipaments.GetOneEquipaments)
+routes.get('/all-equipaments', verifyToken, GetEquipaments.GetAllEquipaments)
+routes.get('/user/equipaments', verifyToken, GetEquipaments.GetByOwnerEquipaments)
 
 
 export default routes
